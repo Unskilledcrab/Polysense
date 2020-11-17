48f47a397e33efd5429538a0c20608834e760db4
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-11-17 01:46:48 -0500

    include project setup. adding bills configuration

---
+++ PS.Web.API/Configurations/BillConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PS.Shared.Models;
+
+namespace PS.Web.API.Configurations
+{
+    public class BillConfiguration : IEntityTypeConfiguration<Bill>
+    {
+        public void Configure(EntityTypeBuilder<Bill> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .HasMaxLength(250);
+        }
+    }
+}
---
+++ PS.Web.API/Migrations/20201117064633_addingBillsConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PS.Web.API.Migrations
+{
+    public partial class addingBillsConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Bill",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Bill",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
