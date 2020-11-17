b525878bb17662ff3b2f571bd4fd4ad098a5ab3c
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-11-17 14:47:19 -0500

    minor updates

---
+++ Merge Development Branch Changes.ps1
@@ -0,0 +1,32 @@
+﻿$env:Path += ";C:\Program Files\Git\cmd\;C:\Program Files\Git\bin\";
+
+$scriptpath = $MyInvocation.MyCommand.Path;
+$dir = Split-Path $scriptpath;
+cd $dir;
+
+$DevelopBranch = "Development";
+$CurrentBranch = (git rev-parse --abbrev-ref HEAD) | Out-String;
+$Seperator = "----------------------------------";
+Write-Output "Current Branch: $CurrentBranch";
+
+Write-Output $Seperator;
+Write-Output "Checking out develop";
+Write-Output $Seperator;
+git checkout $DevelopBranch;
+
+Write-Output $Seperator;
+Write-Output "Gathering changes";
+Write-Output $Seperator;
+git pull;
+
+Write-Output $Seperator;
+Write-Output "Returning to $CurrentBranch branch";
+Write-Output $Seperator;
+git checkout $CurrentBranch.Trim();
+
+Write-Output $Seperator;
+Write-Output "Merging changes";
+Write-Output $Seperator;
+git merge $DevelopBranch;
+
+Start-Sleep -s 10
--- PS.Shared/Models/Bill.cs
+++ PS.Shared/Models/Bill.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace PS.Shared.Models
 {
     public class Bill : BaseEntity
     {
-        public IEnumerable<Politician> Cosponsors { get; set; }
+        public ICollection<Politician> Cosponsors { get; set; }
         public DateTime IntroductionDatetime { get; set; }
         public string Name { get; set; }
         public Politician Sponsor { get; set; }
