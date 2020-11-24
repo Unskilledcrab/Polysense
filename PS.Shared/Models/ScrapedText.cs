41fd5adba63451954c120a1d673795058c4c8c65
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-11-24 00:20:28 -0500

    #14 thinking about this for encoding for ML.Net... need to discuss with the team during the meeting tomorrow

---
+++ PS.Shared/Models/ScrapedText.cs
@@ -0,0 +1,37 @@
+﻿using PS.Shared.Models.Abstractions;
+using System.Collections.Generic;
+
+namespace PS.Shared.Models
+{
+    public class ScraperInput : BaseEntity
+    {
+        public string Website { get; set; }
+        public string ScrapedText { get; set; }
+
+        public int ClassificationEncoding
+        {
+            get
+            {
+                int encoding = 0;
+                foreach (var item in Classification)
+                {
+                    encoding += item.ClassificationEncoding;
+                }
+                return encoding;
+            }
+        }
+
+        public ICollection<ScraperClassification> Classification { get; set; }
+    }
+
+    public class ScraperClassification : BaseEntity
+    {
+        public string Classification { get; set; }
+
+        /// <summary>
+        /// This is last highest classification encoding plus the last highest classification
+        /// encoding (binary encoding)
+        /// </summary>
+        public int ClassificationEncoding { get; set; }
+    }
+}
