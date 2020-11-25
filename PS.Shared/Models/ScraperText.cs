5742584d97cc2b91daa60915466e03d1c0d64847
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-11-24 19:02:46 -0500

    adding the base scraper entity model to be used in #11 and #7 issues

---
+++ PS.Shared/Models/ScraperText.cs
@@ -0,0 +1,10 @@
+﻿using PS.Shared.Models.Abstractions;
+
+namespace PS.Shared.Models
+{
+    public class ScraperText : BaseEntity
+    {
+        public string Website { get; set; }
+        public string Text { get; set; }
+    }
+}
