4a0b87f09b885499f4baa7f1cbc5d0edfb24a00c
theronsterr17 <tom.ron@madmensoftware.com>
2020-11-29 18:19:57 -0500

    #11 Setup initial test for 5 second web scraper. Scrapes the front page of foxnews/politics and returns all the headline text and links to the headline article as a collection of ScraperText objects

--- PS.Web.Scraper/5 Second/Test5SecondScraper.cs
+++ PS.Web.Scraper/5 Second/Test5SecondScraper.cs
@@ -1,22 +1,42 @@
-﻿using HtmlAgilityPack;
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
+using PS.Shared.Models;
 using PS.Web.Scraper.Abstractions;
+using PS.Web.Scraper.Interfaces;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using PS.Web.Scraper.Interfaces;
 
 namespace PS.Web.Scraper._5_Second
 {
     public class Test5SecondScraper : BaseScraper, ITestWebScraper
     {
+        public IList<ScraperText> ScrapeResults { get; set; }
+
         protected override async Task Scrape(HtmlWeb website, ILogger logger, CancellationToken token)
         {
             var watch = Stopwatch.StartNew();
-            logger.LogInformation("About to scrape UB Unlimited");
-
-            var doc = await website.LoadFromWebAsync("https://www.ubunlimited.com");
-
+            logger.LogInformation("About to fox politics Unlimited");
+            ScrapeResults = new List<ScraperText>();
+            string sourceURL = "https://www.foxnews.com/politics";
+            var doc = await website.LoadFromWebAsync(sourceURL);
+            var docNode = doc.DocumentNode;
+            var test = docNode.QuerySelectorAll(".info .title");
+            foreach (var node in test)
+            {
+                var relativeURL = node.QuerySelector("a").Attributes["href"].Value;
+                var nodeURL = sourceURL + relativeURL;
+                var headlinerText = node.InnerText;
+                logger.LogInformation(headlinerText);
+                logger.LogInformation(nodeURL);
+                ScrapeResults.Add(new ScraperText()
+                {
+                    Website = nodeURL,
+                    Text = headlinerText
+                });
+            }
             watch.Stop();
             var elapsedTime = watch.ElapsedMilliseconds;
             logger.LogInformation($"Scrapped UB Unlimited in {elapsedTime} ms");
