5f06fd39e5a24d51f219073a8261dcb571cb67c5
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-12-20 04:15:05 -0500

    Creating an extension project to use to make it easier to update snippets

---
+++ PolysenseExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Polysense Extensions")]
+[assembly: AssemblyDescription("The extensions used in the Polysense project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Polysense")]
+[assembly: AssemblyProduct("PolysenseExtensions")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components. If
+// you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version Minor Version Build Number Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
