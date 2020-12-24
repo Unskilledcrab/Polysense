91d2481ca5690f13e98d797b048ddf7929f0a16a
theronsterr17 <tom.ron@madmensoftware.com>
2020-12-24 16:44:45 -0600

    Added manual bill scraper and selenium nuget packages to new bracnh for scraping gov track for bills

---
+++ PS.Web.Scraper/Manual/ManualBillScraper.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PS.Shared.HttpClients;
+using PS.Web.Scraper.Abstractions;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PS.Web.Scraper.Manual
+{
+    public class ManualBillScraper : BaseScraper
+    {
+        protected override Task Scrape(ScraperTextClient client, HtmlWeb website, ILogger logger, CancellationToken token)
+        {
+            var watch = Stopwatch.StartNew();
+            logger.LogInformation("About to scrape CNN Politics");
+            string sourceURL = "https://www.govtrack.us/congress/bills/browse?status=28,29,32,33&sort=-current_status_date";
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = sourceURL;
+            var enactedLinks = driver.FindElements(By.ClassName("col-xs-10 col-md-11"));
+            foreach (var element in enactedLinks)
+            {
+                var url = element.FindElement(By.TagName("a")).GetAttribute("href");
+            }
+            watch.Stop();
+            var elapsedTime = watch.ElapsedMilliseconds;
+            logger.LogInformation($"Scrapped CNN Politics in {elapsedTime} ms");
+            return Task.CompletedTask;
+        }
+    }
+}
