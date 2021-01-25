6b2910f70b12a1fa53b16fa7de08b2e70ed82bb6
Jeremy Buentello <jb09056@georgiasouthern.edu>
2021-01-24 21:51:27 -0500

    closes #44 all of the cron timer strings are in. #52 still need to work out how the scraper attribute will register the basecronscrapers... closes #54 this is updating the current IoC shared UI injector to get the secret json config files for the syncfusion license

--- PS.Web.Scraper/Attributes/ScraperAttribute.cs
+++ PS.Web.Scraper/Attributes/ScraperAttribute.cs
@@ -5,14 +5,13 @@ namespace PS.Web.Scraper.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public class ScraperAttribute : Attribute
     {
-        private bool isProductionReady = false;
-
         public ScraperAttribute(TimerType timer, bool isProductionReady = false)
         {
             Timer = timer;
-            this.isProductionReady = isProductionReady;
+            IsProductionReady = isProductionReady;
         }
 
+        public bool IsProductionReady { get; set; } = false;
         public TimerType Timer { get; set; }
     }
 }
--- PS.Web.Scraper/Helpers/ScraperInjector.cs
+++ PS.Web.Scraper/Helpers/ScraperInjector.cs
@@ -30,17 +30,35 @@ namespace PS.Web.Scraper.Helpers
             var allScrapers = Assembly.GetAssembly(typeof(BaseScraper)).GetTypes()
                 .Where(t => typeof(BaseScraper).IsAssignableFrom(t) && t.IsAbstract == false);
 
-            var timers = new List<string>();
+            var timers = new List<ScraperHolder>();
             foreach (var scraperType in allScrapers)
             {
                 ScraperAttribute attribute = (ScraperAttribute)Attribute.GetCustomAttribute(scraperType, typeof(ScraperAttribute));
                 if (attribute != null)
                 {
-                    var expression = CronExpressionHelpers.CronMappings.GetValueOrDefault(attribute.Timer);
-                    if (!timers.Contains(expression))
-                        timers.Add(expression);
+                    BaseScraper scaper = Activator.CreateInstance(scraperType) as BaseScraper;
+
+                    string expression = @"*/5 * * * * *";
+                    if (attribute.IsProductionReady)
+                        expression = CronExpressionHelpers.CronMappings.GetValueOrDefault(attribute.Timer);
+
+                    if (!timers.Any(t => t.CronExpression == expression))
+                    {
+                        timers.Add(new ScraperHolder { CronExpression = expression, Scrapers = new List<BaseScraper> { scaper } });
+                    }
+                    else
+                    {
+                        var holder = timers.First(t => t.CronExpression == expression);
+                        holder.Scrapers.Add(scaper);
+                    }
                 }
             }
         }
+    }
+
+    public class ScraperHolder
+    {
+        public string CronExpression { get; set; }
+        public List<BaseScraper> Scrapers { get; set; } = new List<BaseScraper>();
     }
 }
